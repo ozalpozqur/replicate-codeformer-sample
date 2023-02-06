16d171ec021259bf99428e433e4707a7403b3d39
Özgür ÖZALP <ozgur@altogic.com>
2023-02-06 16:05:19 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,18 +3,18 @@ export default function Footer() {
     <footer className="h-header bg-white z-50 fixed left-0 right-0 bottom-0">
       <div className="px-4 h-full container mx-auto">
         <div className="border-t flex justify-center items-center h-full">
-          <p className="text-gray-500">
+          <p className="text-gray-500 text-center">
             Made with ❤️ and powered by{" "}
             <a
               href="https://altogic.com/"
               target="_blank"
-              className="text-indigo-600 font-semibold"
+              className="text-sky-600 font-semibold"
             >
               Altogic
             </a>{" "}
             &{" "}
             <a
-              className="text-indigo-600 font-semibold"
+              className="text-sky-600 font-semibold"
               target="_blank"
               href="https://replicate.com/"
             >
