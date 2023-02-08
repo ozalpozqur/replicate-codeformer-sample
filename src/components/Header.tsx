50fdf538c8a0ab046deeac465ee976d39ce2bc15
Özgür ÖZALP <ozgur@altogic.com>
2023-02-08 16:16:49 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+import { useStore } from "@/store";
+
 export default function Header() {
+  const { reset } = useStore();
   return (
     <header className="h-header border-b flex items-center">
-      <img className="h-10" src="/svg/logo_light.svg" alt="App Logo" />
+      <Link href="/" onClick={reset}>
+        <img className="h-10" src="/svg/logo_light.svg" alt="App Logo" />
+      </Link>
     </header>
   );
 }
